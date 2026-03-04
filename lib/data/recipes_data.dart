33ef1577739a83d063021847654db132190dc5cc
John Doe <johndoe@example.com>
2026-03-04 15:39:23 -0500

    New caesar salad recipe and screen.

--- lib/data/recipes_data.dart
+++ lib/data/recipes_data.dart
@@ -23,4 +23,30 @@ final List<Recipe> sampleRecipes = [
         '6. Mix sauce with cooked spaghetti or pour over the top.\n'
         '7. Serve while hot.',
   ),
+
+  Recipe(
+    name: 'Caesar Salad',
+    imagePath: 'assets/images/salad.jpg',
+    ingredients: [
+      'Green lettuce',
+      'Toasted cubic breads',
+      'Any cheese',
+      'Caesar dressing',
+      'Chopped/sliced vegetables (onions, tomatoes,...)',
+      'Grilled chicken (optional)',
+      'Black pepper',
+      'Sliced olive',
+      'Olive oil',
+      'Boiled eggs (optional)'
+    ],
+    instructions:
+        '1. Wash and chop the green lettuce.\n'
+        '2. Place lettuce in a large bowl.\n'
+        '3. Add the sliced olive along with any other chopped/sliced veggies.\n'
+        '4. Add toasted cubic bread and grated any favorite cheese.\n'
+        '5. Pour Caesar dressing over the salad.\n'
+        '6. Toss gently to coat everything evenly.\n'
+        '7. Add grilled chicken if desired.\n'
+        '8. Sprinkle black pepper and serve fresh.',
+  ),
 ];
